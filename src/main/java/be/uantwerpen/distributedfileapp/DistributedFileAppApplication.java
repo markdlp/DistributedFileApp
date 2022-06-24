25498c726a9168806d61a1002b93fcb2237408e5
Mark Delaportas <delaportas.markos@gmail.com>
2022-06-24 12:00:05 +0200

    initial commit

---
+++ src/main/java/be/uantwerpen/distributedfileapp/DistributedFileAppApplication.java
@@ -0,0 +1,13 @@
+package be.uantwerpen.distributedfileapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedFileAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedFileAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/uantwerpen/distributedfileapp/DistributedFileAppApplicationTests.java
@@ -0,0 +1,13 @@
+package be.uantwerpen.distributedfileapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedFileAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
