f0af23f49da6d8216f1012b4e9b8343c8bf339e3
Markos Delaportas <delaportas.markos@gmail.com>
2022-08-19 22:15:23 +0200

    added node registry functionality (aka nodeMap)

--- src/main/java/be/uantwerpen/distributedfileapp/Controller/NamingController.java
+++ src/main/java/be/uantwerpen/distributedfileapp/Controller/NamingController.java
@@ -2,12 +2,57 @@ package be.uantwerpen.distributedfileapp.Controller;
 
 
 import be.uantwerpen.distributedfileapp.Model.Naming;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
+@Slf4j
 @Controller
 public class NamingController {
 
     Map<Integer, String> nodeMap = Naming.getNodeMap();
+
+    @RequestMapping(value = {"/file-owner"}, method = RequestMethod.GET)
+    public String getFileOwner(@RequestParam(value = "fileName") String fileName){
+
+        short hashedName = Naming.getHash(fileName);
+
+        log.info("File hash is: " + hashedName);
+
+        return Short.toString(hashedName);
+    }
+
+    @RequestMapping(value = {"/add-node"}, method = RequestMethod.POST)
+    public String addNode2Topology(
+            @RequestParam(value = "nodeName") String nodeName,
+            HttpServletRequest request
+    ){
+        log.info("Adding node: " + nodeName
+                + " with ID: " + Naming.getHash(nodeName) + " ...if absent!"
+        );
+        nodeMap.putIfAbsent(
+                (int) Naming.getHash(nodeName),
+                request.getRemoteAddr()
+        );
+
+        return "Node Added!";
+    }
+
+    @RequestMapping(value = {"/del-node"}, method = RequestMethod.DELETE)
+    public String removeNode(@RequestParam(value = "nodeName") String nodeName){
+
+        log.warn("Removing node: " + nodeName
+                + " with ID: " + Naming.getHash(nodeName) + " ...if present!"
+        );
+        nodeMap.remove((int)Naming.getHash(nodeName));
+
+        return "Node Removed!";
+    }
+
+    private void saveMap2LocalDisk(Map<Integer, String> map){
+
+    }
 }
--- src/main/java/be/uantwerpen/distributedfileapp/Model/Naming.java
+++ src/main/java/be/uantwerpen/distributedfileapp/Model/Naming.java
@@ -10,10 +10,12 @@ public class Naming {
         return nodeMap;
     }
 
-    public static Integer getHash(String message){
+    public static short getHash(String message){
 
-        long times2Max = Integer.MAX_VALUE + Math.abs(Integer.MIN_VALUE);
+        // Choosing 32479 instead of 32768 because a prime number spreads the distribution of hash
+        // values better -> [0, 32479] : 289 values left out
+        int tmp = (message.hashCode() % 32479);
 
-        return Math.toIntExact(((message.hashCode() + Integer.MAX_VALUE) * 32768L) / times2Max);
+        return (short) tmp;
     }
 }
