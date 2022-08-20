366e59c85d70d3cb8eac0fa80d153b5c4906cce7
Mark Delaportas <delaportas.markos@gmail.com>
2022-08-21 01:40:31 +0200

    Added file-search functionality and map saving

--- src/main/java/be/uantwerpen/distributedfileapp/Controller/NamingController.java
+++ src/main/java/be/uantwerpen/distributedfileapp/Controller/NamingController.java
@@ -1,35 +1,40 @@
 package be.uantwerpen.distributedfileapp.Controller;
 
-
 import be.uantwerpen.distributedfileapp.Model.Naming;
+import be.uantwerpen.distributedfileapp.Service.NamingService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
 @Slf4j
-@Controller
+@RestController
 public class NamingController {
 
+    NamingService namingService;
     Map<Integer, String> nodeMap = Naming.getNodeMap();
 
     @RequestMapping(value = {"/file-owner"}, method = RequestMethod.GET)
     public String getFileOwner(@RequestParam(value = "fileName") String fileName){
 
         short hashedName = Naming.getHash(fileName);
-
         log.info("File hash is: " + hashedName);
 
-        return Short.toString(hashedName);
+        Integer fileOwner = namingService.getFileOwner((int)hashedName);
+
+        return "IP of the file Owner: " + nodeMap.get(fileOwner);
     }
 
     @RequestMapping(value = {"/add-node"}, method = RequestMethod.POST)
     public String addNode2Topology(
             @RequestParam(value = "nodeName") String nodeName,
             HttpServletRequest request
-    ){
+    ) throws IOException {
         log.info("Adding node: " + nodeName
                 + " with ID: " + Naming.getHash(nodeName) + " ...if absent!"
         );
@@ -38,21 +43,30 @@ public class NamingController {
                 request.getRemoteAddr()
         );
 
+        saveMap2LocalDisk(nodeMap);
+
         return "Node Added!";
     }
 
     @RequestMapping(value = {"/del-node"}, method = RequestMethod.DELETE)
-    public String removeNode(@RequestParam(value = "nodeName") String nodeName){
+    public String removeNode(
+            @RequestParam(value = "nodeName") String nodeName
+    ) throws IOException {
 
         log.warn("Removing node: " + nodeName
                 + " with ID: " + Naming.getHash(nodeName) + " ...if present!"
         );
         nodeMap.remove((int)Naming.getHash(nodeName));
 
+        saveMap2LocalDisk(nodeMap);
+
         return "Node Removed!";
     }
 
-    private void saveMap2LocalDisk(Map<Integer, String> map){
+    private void saveMap2LocalDisk(Map<Integer, String> map) throws IOException {
 
+        ObjectMapper mapper = new ObjectMapper();
+
+        mapper.writeValue(new File("nodeMap.json"), map);
     }
 }
---
+++ src/main/java/be/uantwerpen/distributedfileapp/Service/NamingService.java
@@ -0,0 +1,34 @@
+package be.uantwerpen.distributedfileapp.Service;
+
+import be.uantwerpen.distributedfileapp.Model.Naming;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class NamingService {
+
+    Map<Integer, String> nodeMap = Naming.getNodeMap();
+
+    public Integer getFileOwner(Integer hashedName){
+
+        int diff = 32768;// Biggest possible key difference
+        int ownerID = 0; // Id of the file owner
+
+        Map<Integer, String> N = new ConcurrentHashMap<>();
+
+        for(Map.Entry<Integer, String> entry : nodeMap.entrySet())
+            if(entry.getKey() < hashedName) N.put(entry.getKey(), entry.getValue());
+
+        if (N.isEmpty()){
+            for(Integer nodeKey : nodeMap.keySet()) if(nodeKey > ownerID) ownerID = nodeKey;
+            // Maximum ID in the collection
+            return ownerID;
+
+        } else for(Integer nodeKey: N.keySet()){
+            if (Math.abs(hashedName - nodeKey) < diff){
+                diff = Math.abs(hashedName - nodeKey);
+                ownerID = nodeKey;
+            }
+        }return ownerID;
+    }
+}
